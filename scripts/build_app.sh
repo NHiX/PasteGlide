426402723501eb4da703327c640105cfec389727
nicolasnhix <nicolasnhix@gmail.com>
2026-05-28 15:22:50 +0200

    Add macOS app icon

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -9,15 +9,18 @@ DIST_DIR="${ROOT_DIR}/dist"
 APP_DIR="${DIST_DIR}/${APP_NAME}.app"
 CONTENTS_DIR="${APP_DIR}/Contents"
 MACOS_DIR="${CONTENTS_DIR}/MacOS"
+RESOURCES_DIR="${CONTENTS_DIR}/Resources"
+ICON_FILE="${ROOT_DIR}/Assets/AppIcon.icns"
 
 cd "${ROOT_DIR}"
 
 swift build -c release --product "${APP_BASE_NAME}"
 
 rm -rf "${APP_DIR}"
-mkdir -p "${MACOS_DIR}"
+mkdir -p "${MACOS_DIR}" "${RESOURCES_DIR}"
 
 cp ".build/release/${APP_BASE_NAME}" "${MACOS_DIR}/${APP_BASE_NAME}"
+cp "${ICON_FILE}" "${RESOURCES_DIR}/AppIcon.icns"
 
 cat > "${CONTENTS_DIR}/Info.plist" <<PLIST
 <?xml version="1.0" encoding="UTF-8"?>
@@ -32,6 +35,8 @@ cat > "${CONTENTS_DIR}/Info.plist" <<PLIST
     <string>${APP_BASE_NAME}</string>
     <key>CFBundleIdentifier</key>
     <string>local.pasteglide.${STAMP}</string>
+    <key>CFBundleIconFile</key>
+    <string>AppIcon</string>
     <key>CFBundleInfoDictionaryVersion</key>
     <string>6.0</string>
     <key>CFBundleName</key>
